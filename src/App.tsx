1478ebad2c954e8ac75cdfc13cf4973f09f494cb
airtoxin <airtoxin@icloud.com>
2019-07-15 07:26:43 +0900

    Simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,27 @@
 import React from "react";
 import "./App.css";
-import { DndProvider, useDragLayer } from "react-dnd";
-import Backend from "react-dnd-touch-backend";
-import { useDrag, useDrop } from "react-dnd";
+import { css, Global } from "@emotion/core";
+import normalize from "emotion-normalize";
+import { Card } from "./components/Card";
 
 export const App: React.FC = () => {
   return (
     <div className="App">
-      <DndProvider backend={Backend}>
-        <DragLayer />
-        <Draggable name={"dayo"} />
-        <Draggable name={"dayo2"} />
-        <Droppable />
-      </DndProvider>
-    </div>
-  );
-};
-
-const DragLayer: React.FunctionComponent = () => {
-  const { item, offset } = useDragLayer(monitor => ({
-    item: monitor.getItem(),
-    offset: monitor.getSourceClientOffset()
-  }));
-
-  if (!item || !offset) return null;
-
-  return (
-    <div
-      style={{
-        transform: `translate(${offset.x}px, ${offset.y}px)`
-      }}
-    >
-      text {item.id}
-    </div>
-  );
-};
-
-const Draggable: React.FunctionComponent<{ name: string }> = ({ name }) => {
-  const [{ opacity }, dragRef] = useDrag({
-    item: { type: "CARD", id: name },
-    end: console.log,
-    collect: monitor => ({
-      opacity: monitor.isDragging() ? 0.5 : 1
-    })
-  });
-
-  return (
-    <div ref={dragRef} style={{ opacity }}>
-      text {name}
-    </div>
-  );
-};
-
-const Droppable: React.FunctionComponent = () => {
-  const [{ isOver, canDrop }, dropRef] = useDrop({
-    accept: "CARD",
-    drop: console.log,
-    collect: monitor => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop()
-    })
-  });
-  return (
-    <div
-      ref={dropRef}
-      style={{ width: 300, height: 300, margin: 10, border: "solid 1px" }}
-    >
-      {isOver && "OVER"}
-      {canDrop && "DROP HERE!!!!!!!!"}
+      <Global
+        styles={css`
+          ${normalize}
+          html, body {
+            padding: 0;
+            margin: 0;
+          }
+        `}
+      />
+      <div style={{ display: "flex" }}>
+        <Card onClick={console.log}>1</Card>
+        <Card onClick={console.log}>5</Card>
+        <Card onClick={console.log}>10</Card>
+        <Card onClick={console.log}>100</Card>
+      </div>
     </div>
   );
 };
---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export const Card = styled.div({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  border: "solid 1px",
+  width: "3rem",
+  height: "3rem"
+});
